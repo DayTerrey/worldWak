05b77890b99f9cc9e2e2a68adfaf623c4947eee1
Robert <rob.garaev2000@yandex.ru>
2024-10-27 16:50:21 +0300

    Update page.tsx
    add

--- app/page.tsx
+++ app/page.tsx
@@ -8,6 +8,7 @@ import { button as buttonStyles } from "@nextui-org/theme";
 export default function Home() {
   return null;
   // (
+  //sfsfds
   //   <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
   //     {/* <p>Новый текст</p> */}
   //   </section>
